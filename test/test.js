dab3cccdc2ea6cd77ac4f954646022512125f0bb
slikts <dabas@untu.ms>
2017-05-19 14:47:19 +0300

    Fixtures

--- test/test.js
+++ test/test.js
@@ -3,39 +3,39 @@ const path = require('path')
 
 const { parseFile, filterCalls, parseNode, getArg, readFile, extractData } = require('..')
 
-const asset = filename => path.join('test/assets', filename)
+const fixture = filename => path.join('test/fixtures', filename)
 
 test('parse file', t => {
-  const ast = parseFile(asset('recipe.lua'))
+  const ast = parseFile(fixture('recipe.lua'))
   t.equal(ast.type, 'Chunk')
   t.end()
 })
 
 test('filter calls', t => {
-  const { body } = parseFile(asset('recipe.lua'))
+  const { body } = parseFile(fixture('recipe.lua'))
   const [{ type }] = filterCalls(body)
   t.equal(type, 'CallStatement')
   t.end()
 })
 
 test('get arg', t => {
-  const [call] = filterCalls(parseFile(asset('recipe.lua')).body)
+  const [call] = filterCalls(parseFile(fixture('recipe.lua')).body)
   const { type } = getArg(call)
   t.equal(type, 'TableConstructorExpression')
   t.end()
 })
 
 test('parse node', t => {
-  const [call] = filterCalls(parseFile(asset('recipe.lua')).body)
+  const [call] = filterCalls(parseFile(fixture('recipe.lua')).body)
   const data = parseNode(getArg(call))
   const { stringify, parse } = JSON
-  t.equal(stringify(data), stringify(parse(readFile('test/assets/recipe.json'))))
+  t.equal(stringify(data), stringify(parse(readFile(fixture('recipe.json')))))
   t.end()
 })
 
 test('extract data', t => {
   t.plan(1)
-  const data = extractData(asset('recipe.lua'))
+  const data = extractData(fixture('recipe.lua'))
   const { stringify, parse } = JSON
-  t.equal(stringify(data), stringify([parse(readFile(asset('recipe.json')))]))
+  t.equal(stringify(data), stringify([parse(readFile(fixture('recipe.json')))]))
 })
